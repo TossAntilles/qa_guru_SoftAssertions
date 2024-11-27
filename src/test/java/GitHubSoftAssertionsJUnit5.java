cf58d1d3600b73e0918046d469880611008d33aa
Anton Bogutskii <schicksalkreuzung@gmail.com>
2024-11-27 23:40:05 +0300

    Added one more case for Pages list Made code block validation easier to read

--- src/test/java/GitHubSoftAssertionsJUnit5.java
+++ src/test/java/GitHubSoftAssertionsJUnit5.java
@@ -16,20 +16,36 @@ public class GitHubSoftAssertionsJUnit5 {
         Configuration.baseUrl = "https://github.com/selenide/selenide";
     }
 
-
     @Test
-    void gitHubSoftAssertionsLink() {
+    void gitHubSoftAssertionsLinkFullList() {
 
         // Opening Selenide wiki page
         open(""); //remove
         $("#wiki-tab").click();
 
-        $("#wiki-pages-box .js-wiki-more-pages-link").click();
+        // Search by expanding link list
+        $("#wiki-pages-box .js-wiki-more-pages-link").click(); // Expanding list
         $("#wiki-pages-box").shouldHave(text("SoftAssertions")); // Link is displayed
         $("#wiki-pages-box").$(byText("SoftAssertions")).
                 shouldHave(attribute("href", "https://github.com/selenide/selenide/wiki/SoftAssertions")); // Correct link value
-        System.out.println("[PASSED] Wiki main page: SoftAssertions Link exists");
 
+        System.out.println("[PASSED] Wiki main page: SoftAssertions Link exists [expanded list]");
+    }
+
+    @Test
+    void gitHubSoftAssertionsLinkAfterSearcht() {
+
+        // Opening Selenide wiki page
+        open(""); //remove
+        $("#wiki-tab").click();
+
+        // Search by filtering link list
+        $("#wiki-pages-box #wiki-pages-filter").append("SoftAssertions"); // Filtering list
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions")); // Link is displayed
+        $("#wiki-pages-box").$(byText("SoftAssertions")).
+                shouldHave(attribute("href", "https://github.com/selenide/selenide/wiki/SoftAssertions")); // Correct link value
+
+        System.out.println("[PASSED] Wiki main page: SoftAssertions Link exists [search field]");
     }
 
     @Test
@@ -41,12 +57,15 @@ public class GitHubSoftAssertionsJUnit5 {
         // Searching code block after the JUnit5 header
         // Current wiki structure hase code block right after the header => checking sibling div
         $(withText("Using JUnit5 extend test class:")).shouldBe(visible);
-        // Should be a code block
-        $(withText("Using JUnit5 extend test class:")).parent().sibling(0).shouldHave(cssClass("highlight-source-java"));
         $(withText("Using JUnit5 extend test class:")).parent().sibling(0).
-                shouldHave(text("@ExtendWith({SoftAssertsExtension.class}) class Tests { " +
-                        "@Test void test() { Configuration.assertionMode = SOFT; open(\"page.html\"); " +
-                        "$(\"#first\").should(visible).click(); $(\"#second\").should(visible).click(); } }"));
+                shouldHave(text("@ExtendWith({SoftAssertsExtension.class}) " +
+                        "class Tests { " +
+                        "@Test void test() { Configuration.assertionMode = SOFT;" +
+                        " open(\"page.html\"); " +
+                        "$(\"#first\").should(visible).click(); " +
+                        "$(\"#second\").should(visible).click(); " +
+                        "} }"));
+
         System.out.println("[PASSED] SoftAssertions page: JUnit5 code exists");
     }
 
